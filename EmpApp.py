5459098f1249a59454bb8c61d563be2ee12794a4
wenjet <72378168+WenJet01@users.noreply.github.com>
2022-09-27 04:28:59 +0800

    Merge pull request #8 from WenJet01/braaa
    Braaa

--- EmpApp.py
+++ EmpApp.py
@@ -1,9 +1,14 @@
+from asyncio.windows_events import NULL
 from sqlite3 import Cursor
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
 from config import *
+from datetime import datetime
+
+
+
 
 
 app = Flask(__name__)
@@ -27,6 +32,10 @@ cursor = db_conn.cursor()
 def home():
     return render_template('index.html')
 
+@app.route("/portfolio", methods=['GET', 'POST'])
+def portfolio():
+    return render_template('portfolio.html', bucket=bucket)
+
 
 @app.route("/about", methods=['POST'])
 def about():
@@ -40,9 +49,10 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
+    salary_rate = request.form['salary_rate']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, NULL)"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
@@ -50,11 +60,11 @@ def AddEmp():
 
     try:
 
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, salary_rate))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file.jpg"
         s3 = boto3.resource('s3')
 
         try:
@@ -87,7 +97,7 @@ def directory():
     cursor.execute("SELECT * FROM employee")
     directoryData = cursor.fetchall()
     print(directoryData)
-    return render_template('directory.html', data = directoryData)
+    return render_template('directory.html', data = directoryData, bucket=bucket)
 
 @app.route('/editProfile', methods =['GET', 'POST'])
 def editProfile():
@@ -98,6 +108,19 @@ def editProfile():
         directoryData = cursor.fetchone()
         print(directoryData)
         return render_template('editProfile.html', data = directoryData)
+
+@app.route('/profile', methods =['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+        data = request.form["input"]
+        print(data)
+        cursor.execute("SELECT * FROM employee WHERE emp_id="+data)
+        directoryData = cursor.fetchone()
+        cursor.execute("SELECT * FROM performanceNote WHERE emp_id="+data)
+        performanceData = cursor.fetchall()
+        print(directoryData)
+        print(performanceData)
+        return render_template('profile.html', data = directoryData, bucket=bucket, performance = performanceData)
 
 @app.route("/saveProfile", methods=['POST'])
 def saveProfile():
@@ -121,7 +144,7 @@ def saveProfile():
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file.jpg"
         s3 = boto3.resource('s3')
 
         try:
@@ -144,14 +167,50 @@ def saveProfile():
             return str(e)
 
     finally:
-        cursor.execute("SELECT * FROM employee")
-        directoryData = cursor.fetchall()
+        cursor.execute("SELECT * FROM employee WHERE emp_id="+emp_id)
+        directoryData = cursor.fetchone()
 
     print("all modification done...")
-    
-    
-    return render_template('directory.html', data = directoryData)
+    return render_template('profile.html', data = directoryData)
+
+@app.route('/deleteProfile', methods =['GET', 'POST'])
+def deleteProfile():
+    if request.method == 'POST':
+        data = request.form["input"]
+        print(data)
+        cursor.execute("DELETE FROM employee WHERE emp_id="+data)
+        return render_template('deletedProfile.html', data = data)
+
+@app.route('/performanceAdd', methods =['GET', 'POST'])
+def performanceAdd():
+    today = datetime.now()
+    if request.method == 'POST':
+        data = request.form["input"]
+        print(data)
+        return render_template('performanceAdd.html', data = data)
+
+@app.route('/performanceSave', methods =['GET', 'POST'])
+def performanceSave():
+    today = datetime.now()
+    todayDate = today.strftime("%Y-%m-%d")
+    idDate = today.strftime("%y%m%d%H%M%S")
+    if request.method == 'POST':
+        emp_id = request.form["emp_id"]
+        note = request.form["note"]
+        date = todayDate
+        id= str("PN" + idDate)
+        print(id)
+        print(date)
+        print(note)
+        print(emp_id)
+        update_sql = "INSERT INTO performanceNote VALUES (%s, %s, %s, %s)"
+
+        cursor.execute(update_sql, (id,date,note,emp_id))
+        db_conn.commit()
+
+        return render_template('performanceSaved.html' )
       
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+    
