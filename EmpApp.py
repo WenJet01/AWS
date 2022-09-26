3a167cdec78422eb2f3219ee65c055eee10de33e
wenjet <72378168+WenJet01@users.noreply.github.com>
2022-09-27 04:29:26 +0800

    1

--- EmpApp.py
+++ EmpApp.py
@@ -118,9 +118,11 @@ def profile():
         directoryData = cursor.fetchone()
         cursor.execute("SELECT * FROM performanceNote WHERE emp_id="+data)
         performanceData = cursor.fetchall()
+        cursor.execute("SELECT * FROM cert WHERE emp_id="+data)
+        certData = cursor.fetchall()
         print(directoryData)
         print(performanceData)
-        return render_template('profile.html', data = directoryData, bucket=bucket, performance = performanceData)
+        return render_template('profile.html', data = directoryData, bucket=bucket, performance = performanceData, cert = certData)
 
 @app.route("/saveProfile", methods=['POST'])
 def saveProfile():
@@ -181,6 +183,13 @@ def deleteProfile():
         cursor.execute("DELETE FROM employee WHERE emp_id="+data)
         return render_template('deletedProfile.html', data = data)
 
+@app.route('/performance', methods =['GET', 'POST'])
+def performance():
+    cursor.execute("SELECT * FROM performanceNote")
+    directoryData = cursor.fetchall()
+    print(directoryData)
+    return render_template('performanceList.html', data = directoryData)
+
 @app.route('/performanceAdd', methods =['GET', 'POST'])
 def performanceAdd():
     today = datetime.now()
@@ -188,6 +197,15 @@ def performanceAdd():
         data = request.form["input"]
         print(data)
         return render_template('performanceAdd.html', data = data)
+
+@app.route('/performanceDetail', methods =['GET', 'POST'])
+def performanceDetail():
+    if request.method == 'POST':
+        data = request.form["input"]
+        cursor.execute("SELECT * FROM performanceNote WHERE note_id = '"+data+"'")
+        directoryData = cursor.fetchone()
+        print(directoryData)
+        return render_template('performanceDetail.html', data = directoryData)
 
 @app.route('/performanceSave', methods =['GET', 'POST'])
 def performanceSave():
@@ -208,7 +226,89 @@ def performanceSave():
         cursor.execute(update_sql, (id,date,note,emp_id))
         db_conn.commit()
 
-        return render_template('performanceSaved.html' )
+        return render_template('success.html', data = id )
+
+@app.route('/performanceEditSave', methods =['GET', 'POST'])
+def performanceEditSave():
+    if request.method == 'POST':
+        emp_id = request.form["emp_id"]
+        note = request.form["note"]
+        date = request.form["date"]
+        id= request.form["id"]
+        print(id)
+        print(date)
+        print(note)
+        print(emp_id)
+        update_sql = "UPDATE performanceNote SET date = %s, note = %s, emp_id = %s WHERE note_id = %s"
+
+        cursor.execute(update_sql, (date,note,emp_id,id))
+        db_conn.commit()
+
+        return render_template('success.html',data = id )
+
+# cert
+@app.route('/cert', methods =['GET', 'POST'])
+def cert():
+    cursor.execute("SELECT * FROM cert")
+    directoryData = cursor.fetchall()
+    print(directoryData)
+    return render_template('certList.html', data = directoryData)
+
+@app.route('/certAdd', methods =['GET', 'POST'])
+def certAdd():
+    today = datetime.now()
+    if request.method == 'POST':
+        data = request.form["input"]
+        print(data)
+        return render_template('certAdd.html', data = data)
+
+@app.route('/certDetail', methods =['GET', 'POST'])
+def certDetail():
+    if request.method == 'POST':
+        data = request.form["input"]
+        cursor.execute("SELECT * FROM cert WHERE cert_id = '"+data+"'")
+        directoryData = cursor.fetchone()
+        print(directoryData)
+        return render_template('certDetail.html', data = directoryData)
+
+@app.route('/certSave', methods =['GET', 'POST'])
+def certSave():
+    today = datetime.now()
+    todayDate = today.strftime("%Y-%m-%d")
+    idDate = today.strftime("%y%m%d%H%M%S")
+    if request.method == 'POST':
+        emp_id = request.form["emp_id"]
+        name = request.form["name"]
+        date = request.form["date"]
+        given_by = request.form["given_by"]
+        id= str("CT" + idDate)
+        
+        update_sql = "INSERT INTO cert VALUES (%s, %s, %s, %s, %s)"
+
+        cursor.execute(update_sql, (id,date,name,given_by,emp_id))
+        db_conn.commit()
+
+        return render_template('success.html', data = id )
+
+@app.route('/certEditSave', methods =['GET', 'POST'])
+def certEditSave():
+    if request.method == 'POST':
+        emp_id = request.form["emp_id"]
+        name = request.form["name"]
+        date = request.form["date"]
+        given_by = request.form["given_by"]
+        id= request.form["id"]
+        print(id)
+        print(date)
+        print(name)
+        print(emp_id)
+        update_sql = "UPDATE cert SET date = %s, name = %s, given_by = %s, emp_id = %s WHERE cert_id = %s"
+
+        cursor.execute(update_sql, (date,name,given_by,emp_id,id))
+        db_conn.commit()
+
+        return render_template('success.html',data = id )
+      
       
 
 if __name__ == '__main__':
